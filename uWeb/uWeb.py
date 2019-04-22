996aeea879cb577da9b20caa71a6fac3c414560f
petabite <jmslca123@gmail.com>
2019-04-21 19:37:39 -0700

    add render

--- uWeb/uWeb.py
+++ uWeb/uWeb.py
@@ -1,50 +1,52 @@
-try:
-    import usocket as socket
-except:
-    import socket
+import usocket as socket
 
+class uWeb:
 
-CONTENT = b"""\
-HTTP/1.0 200 OK
+    def __init__(self):
 
-<h1>Hello #%d from MicroPython!</h1>
-"""
+        self.active_socket = socket.socket()
 
-def main(micropython_optimize=False):
-    s = socket.socket()
+        self.address_info = socket.getaddrinfo("0.0.0.0", 8080)
+        self.address = self.address_info[0][-1]
+        print("Bind address info:", self.address_info)
 
-    ai = socket.getaddrinfo("0.0.0.0", 8080)
-    print("Bind address info:", ai)
-    addr = ai[0][-1]
+        self.active_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.active_socket.bind(self.address)
+        self.active_socket.listen(5)
 
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(addr)
-    s.listen(5)
-    print("Listening, connect your browser to http://<this_host>:8080/")
+    def start(self):
+        print("Listening, connect your browser to http://<this_host>:8080/")
 
-    counter = 0
-    while True:
-        res = s.accept()
-        client_sock = res[0]
-        client_addr = res[1]
-        print("Client address:", client_addr)
-        print("Client socket:", client_sock)
-
-        print("Request:")
-        req = client_sock.readline()
-        print(req)
-        print('after req')
+        counter = 0
         while True:
-            h = client_sock.readline()
-            if h == b"" or h == b"\r\n":
-                break
-            print(h)
-        client_sock.write(CONTENT % counter)
+            connection = self.active_socket.accept()
+            self.client_socket = connection[0]
+            self.client_address = connection[1]
+            print("Client address:", self.client_address)
+            print("Client socket:", self.client_socket)
 
-        client_sock.close()
+            print("Client Request:")
+            request_line = self.client_socket.readline()
+            print(request_line)
+            while True:
+                h = self.client_socket.readline()
+                if h == b"" or h == b"\r\n":
+                    break
+                print(h)
+            print('after req')
 
-        counter += 1
-        print()
+            self.render('content.html', {"counter":counter})
+            self.client_socket.close()
 
+            counter += 1
+            print()
 
-main()
+    def render(self, html, variables):
+        rendered_content = content
+        for var_name, value in variables.items():
+            rendered_content = rendered_content.replace(b"{{%s}}" % var_name, str(value).encode())
+
+        self.client_socket.write(rendered_content)
+
+server = uWeb()
+server.start()
